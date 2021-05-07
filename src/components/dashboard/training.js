c637d229b61cac08fa7ad37f07f23bd73b96dd5d
alihassan <24819103+thisalihassan@users.noreply.github.com>
2021-05-07 19:38:42 +0500

    api connected and training plans working

--- src/components/dashboard/training.js
+++ src/components/dashboard/training.js
@@ -1,59 +1,156 @@
-import React ,{Fragment ,useEffect, useState} from 'react';
-import Breadcrumb from '../common/breadcrumb';
-import { MDBDataTableV5 } from 'mdbreact';
-import customerActions from "../../redux/customers/actions"
-import axios from 'axios';
-import { API_URL, CONFIG } from '../../services/helper';
+import React, { Fragment, useEffect, useState } from 'react';
+import trainingPlansActions from '../../redux/trainingPlans/actions';
 import { connect } from 'react-redux';
-const { fetchCustomers } = customerActions;
+const { createTrainingPlan, fetchTrainingPlans } = trainingPlansActions;
 
-const Training = () => {
-    const [inputs, setInputs] = useState({ inputs: ['Day-1'] })
+const Training = ({ user, createTrainingPlan, fetchTrainingPlans }) => {
+	const [inputs, setInputs] = useState({ inputs: ['Exercise-1'] });
+	const [formData, setFormData] = useState({
+		planName: '',
+		exercises: [{ set: '', weight: '', exercise: '', repetitions: '', comment: '', id: '' }]
+	});
 
-    const appendInput = () => {
-        var newInput = `Day-${inputs.inputs.length+1}`;
-        setInputs(prevState => ({ inputs: prevState.inputs.concat([newInput]) }));
-    }
-    return (
-        <div className= {'container-fluid p-0'}>
-            <form className='theme-form'>
-            {inputs.inputs.map(input => {
-                return (
-                    <>
-                        <label className='col-form-label pt-0'>{input}</label>
-                        <div className='form-group'>
-                        <input
-                            className='form-control'
-                            type='text'
-                            name={`${input}-sets`}
-                            placeholder='sets'
-                        />
-                        <input
-                            className='form-control'
-                            type='text'
-                            name={`${input}-weights`}
-                            placeholder='weights'
-                        />
-                        <input
-                            className='form-control'
-                            type='comments'
-                            name={`${input}-comments`}
-                            placeholder='comments'
-                        />
-                    </div>
-                    </>
-                    
-                )
-            })}
-                
-                
-            </form>
-											
-            <button onClick={ appendInput }>
-                CLICK ME TO ADD AN INPUT
-            </button>
-        </div>
-  );
+	useEffect(() => {
+		if (user) {
+			console.log(user._id);
+			fetchTrainingPlans(user._id);
+		}
+	}, [fetchTrainingPlans, user]);
+
+	const removeExercise = (event, index) => {
+		const { exercises } = formData;
+		event.preventDefault();
+		exercises.splice(index, 1);
+
+		inputs.inputs.splice(index, 1);
+		setInputs({ inputs: inputs.inputs });
+		setFormData((prevState) => ({ ...prevState, exercises }));
+	};
+
+	const addNewExercise = (index, e) => {
+		const { exercises } = formData;
+		const { name, value } = e.target;
+		if (index <= exercises.length - 1) {
+			exercises[index][name] = value;
+		} else {
+			exercises.push({
+				name: value
+			});
+		}
+		setFormData({ ...formData, exercises });
+	};
+
+	const submitForm = () => {
+		const { planName, exercises } = formData;
+		const payload = [];
+
+		for (let i = 0; i < exercises.length; i++) {
+			const body = {};
+			body.user = user._id;
+			body.planName = planName;
+			body.set = exercises[i].set;
+			body.weight = exercises[i].weight;
+			body.exercise = exercises[i].exercise;
+			body.repetitions = exercises[i].repetitions;
+			body.comment = exercises[i].comment;
+			payload.push(body);
+		}
+		createTrainingPlan(payload);
+	};
+
+	const appendInput = () => {
+		var newInput = `Exercise-${inputs.inputs.length + 1}`;
+		setInputs((prevState) => ({ inputs: prevState.inputs.concat([newInput]) }));
+	};
+
+	const { exercises } = formData;
+
+	return (
+		<div className={'container-fluid p-0'}>
+			<form className='theme-form'>
+				<label className='col-form-label pt-0'>Training Plan Name</label>
+				<div className='form-group'>
+					<input
+						onChange={(e) => setFormData((prevState) => ({ ...prevState, planName: e.target.value }))}
+						required
+						className='form-control'
+						type='text'
+						name='planName'
+						placeholder='sets'
+					/>
+				</div>
+				{inputs.inputs.map((input, index) => {
+					return (
+						<Fragment key={input + index}>
+							<label style={{ float: 'left' }} className='col-form-label pt-0'>
+								{input}
+							</label>
+							{index !== 0 && (
+								<label onClick={(e) => removeExercise(e, index)} style={{ float: 'right' }} className='col-form-label pt-0'>
+									X
+								</label>
+							)}
+							<div className='form-group'>
+								<input
+									onChange={(e) => addNewExercise(index, e)}
+									required
+									value={exercises[index] && exercises[index].exercise}
+									className='form-control'
+									type='text'
+									name='exercise'
+									placeholder='Exercise Name'
+								/>
+								<input
+									onChange={(e) => addNewExercise(index, e)}
+									required
+									value={exercises[index] && exercises[index].set}
+									className='form-control'
+									type='text'
+									name='set'
+									placeholder='sets'
+								/>
+								<input
+									onChange={(e) => addNewExercise(index, e)}
+									required
+									value={exercises[index] && exercises[index].weight}
+									className='form-control'
+									type='text'
+									name='weight'
+									placeholder='weights'
+								/>
+								<input
+									onChange={(e) => addNewExercise(index, e)}
+									required
+									className='form-control'
+									type='text'
+									value={exercises[index] && exercises[index].repetitions}
+									name='repetitions'
+									placeholder='Repetitions'
+								/>
+								<input
+									onChange={(e) => addNewExercise(index, e)}
+									required
+									className='form-control'
+									type='comments'
+									value={exercises[index] && exercises[index].comment}
+									name='comment'
+									placeholder='comments'
+								/>
+							</div>
+						</Fragment>
+					);
+				})}
+				<span onClick={submitForm}>Submit Training Plan</span>
+			</form>
+
+			<button onClick={appendInput}>CLICK ME TO ADD AN INPUT</button>
+		</div>
+	);
 };
 
-export default Training;
+export default connect(
+	(state) => ({
+		user: state.authReducer.user
+	}),
+	{ createTrainingPlan, fetchTrainingPlans }
+)(Training);
--- src/redux/index.js
+++ src/redux/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import Customizer from './customizer/reducer';
-import authReducer from './auth/reducer'
-import customerReducer from './customers/reducer'
+import authReducer from './auth/reducer';
+import customerReducer from './customers/reducer';
+import plans from './trainingPlans/reducer';
 const reducers = combineReducers({
+	plans,
 	Customizer,
 	authReducer,
 	customerReducer
