e0540a68d5988ada30154fd3f87e934821d78412
alihassan <24819103+thisalihassan@users.noreply.github.com>
2021-05-09 15:57:50 +0500

    removed unused packages

--- src/redux/reducer.js
+++
@@ -1,9 +0,0 @@
-
-import { REHYDRATE } from 'redux-persist/lib/constants'; 
-
-export default function reducer(state= {}, action){
-switch(action.type){
-    case REHYDRATE:
-      return { ...state, persistedState: action.payload };
-    break;
-}}
