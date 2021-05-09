fc5799cbb1438d69053d53bde93cd7877e6b66d2
alihassan <24819103+thisalihassan@users.noreply.github.com>
2021-05-10 01:42:20 +0500

    update

--- src/services/helper.js
+++ src/services/helper.js
@@ -1,4 +1,5 @@
-export const API_URL = 'https://fitportal-backend.herokuapp.com'
+export const API_URL =
+	process.env.NODE_ENV === 'production' ? 'https://fitportal-backend.herokuapp.com' : 'http://localhost:6600';
 export const CONFIG = {
 	headers: {
 		'Content-Type': 'application/json'
